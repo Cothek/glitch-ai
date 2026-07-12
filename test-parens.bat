533453bec5c334292e4f41867f2bf997da7c8b44
cothek <cothek@gmail.com>
2026-07-12 13:00:32 -0500

    chore: remove test artifacts from bootstrap changes

--- test-parens.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not exist "%~dp0data\node\node.exe" (
-    where node >nul 2>nul
-    if errorlevel 1 (
-        echo Bootstrapping Glitch (first-time setup) with parens...
-    )
-)
-echo Done
