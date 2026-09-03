2d7605e5977f989c94118243d44f1c391476cf6e
cothek <cothek@gmail.com>
2026-09-03 09:16:15 -0500

    chore: remove tracked junk files now quarantined to data/quarantine/

--- check-liveness.js
+++
@@ -1,4 +0,0 @@
-const { isSessionToolRunning } = require('./scripts/lib/agent-watchdog-helpers.mjs');
-
-const result = isSessionToolRunning('ses_fe89aaad0ffeYAaimbyUJqoOVQ');
-console.log('isSessionToolRunning result:', JSON.stringify(result, null, 2));
